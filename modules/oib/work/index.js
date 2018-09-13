09f2d8e17b97dc21bd190aa99dc5720c7344d7b6
liyongyao <125487767@qq.com>
2018-09-13 11:31:50 +0800

    [bug fix]works page bug fix

--- modules/oib/work/index.js
+++ modules/oib/work/index.js
@@ -16,7 +16,7 @@ router.get('/works', (req, res, next) => {
   console.log(req.route.stack[0].method, req.route.path, 'success');
   const { enable, page, rows } = req.query;
   const query = enable && { enable: parseInt(enable) } || {};
-  db.find(query).sort({ sort: -1 }).skip(page - 1).limit(rows).exec(handleErr(res, (content) => {
+  db.find(query).sort({ sort: -1 }).skip((page - 1)*rows).limit(+rows).exec(handleErr(res, (content) => {
     db.count(query, handleErr(res, (totalElements) => {
       handleSuccess(res)({
         totalElements,
@@ -24,6 +24,8 @@ router.get('/works', (req, res, next) => {
       });
     }));
   }));
+
+
 });
 
 
